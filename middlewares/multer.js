7ff57ef16b85d3b4e9662b2e25be77819ebdc3e7
naggar <72861717+naggarx@users.noreply.github.com>
2024-06-29 20:10:34 +0300

    edit 2

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -16,7 +16,7 @@ const upload = multer({
     fileFilter: function(req, file, cb) {
         checkFileType(file, cb);
     }
-}).single('profileImage');
+});
 
 // Check file type
 function checkFileType(file, cb) {
